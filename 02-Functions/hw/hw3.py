9e7601e7c0a49bca2a500bfb64281358ef9642d7
ataluzz <39642607+ataluzz@users.noreply.github.com>
2019-12-02 17:45:28 +0300

    Update hw3.py

--- 02-Functions/hw/hw3.py
+++ 02-Functions/hw/hw3.py
@@ -1,8 +1,14 @@
-counter_name = 0
-
-def make_it_count(func, counter_name):
+def make_it_count(func, counter_name: str):
     def function():
-        global counter_name
-        counter_name += 1
+        globals()[counter_name] += 1
         func()
     return function
+
+def func1():
+    print("Hello, it's me")
+
+asd = 0
+funct = make_it_count(func1, 'asd')
+for i in range(10):
+    funct()
+    print(asd)

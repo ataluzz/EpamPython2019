c08cfef7f5aeb82fdd861c30a4ad201f40870099
ataluzz <39642607+ataluzz@users.noreply.github.com>
2019-12-16 09:52:33 +0300

    Update task1.py

--- 06-advanced-python/hw/task1.py
+++ 06-advanced-python/hw/task1.py
@@ -4,15 +4,40 @@ E - dict(<V> : [<V>, <V>, ...])
 значение - список вершин, достижимых из данной
 Сделать так, чтобы по графу можно было итерироваться(обходом в ширину)
 """
+from queue import Queue
+import collections
 
-
-class Graph:
+class GraphIterator(collections.abc.Iterator):
+    def __init__(self, graph):
+        self.E = E
+        self.queue = Queue()
+        self.keys = list(E.keys())
+        self.visited = [self.keys[0]]
+        self.queue.put(self.keys[0])
+        
+    def __next__(self):
+        while self.queue:
+            k = self.queue.get()
+            values = self.E[k]
+            for val in values:
+                if val not in self.visited:
+                    self.visited.append(val)
+                    self.queue.put(val)
+            return k
+        raise StopIteration
+                    
+                
+                
+class Graph(collections.abc.Iterable):
     def __init__(self, E):
         self.E = E
+    
+    def __iter__(self):
+        return GraphIterator(self)
 
 
 E = {'A': ['B', 'C', 'D'], 'B': ['C'], 'C': [], 'D': ['A']}
 graph = Graph(E)
 
-for vertex in graph:
-    print(vertex)
+for vertice in graph:
+    print(vertice)

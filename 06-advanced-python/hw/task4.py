099ca467aff8c3d5afb32dabe11224699fb5fc08
ataluzz <39642607+ataluzz@users.noreply.github.com>
2019-12-16 17:17:59 +0300

    Update task4.py

--- 06-advanced-python/hw/task4.py
+++ 06-advanced-python/hw/task4.py
@@ -12,20 +12,53 @@ V folder1
 True
 """
 
-
 class PrintableFolder:
-    def __init__(self, name, content):
+    
+    def __init__(self, name, content = None):
         self.name = name
         self.content = content
 
-    def __str__(self):
-        pass
-
-
+    def __str__(self, depth = 0):
+        string = ''
+        if isinstance(self.content, PrintableFile):
+            string = f'{string}\n {"|   " * depth}|-> {self.content.__str__(depth + 1)}'
+        else:
+            for item in self.content:
+                string = f'{string}\n {"|   " * depth}|-> {item.__str__(depth + 1)}'
+        return f'V {self.name}{string}'
+    
+     
+    def __contains__(self, file_name):
+        result = False
+        contents = list(self.content)
+        while contents:
+            item = contents.pop()
+            if isinstance(item, PrintableFolder):
+                try:
+                    contents.extend(item.content)
+                except TypeError:
+                    contents.append(item.content)
+            elif file_name == item:
+                result = True
+        return result
+                
 class PrintableFile:
     def __init__(self, name):
         self.name = name
 
-    def __str__(self):
-        pass
+    def __str__(self, depth = 0):
+        return f'{self.name}'
+ 
 
+file1 = PrintableFile('file1')
+file2 = PrintableFile('file2')
+file3 = PrintableFile('file3')
+file4 = PrintableFile('file4')
+folder4 = PrintableFolder('folder4', [])
+folder3 = PrintableFolder('folder3', [folder4, file3, file4])
+folder2 = PrintableFolder('folder2', [folder3, file2])
+folder1 = PrintableFolder('folder1', [folder2, file1])
+
+print(folder1)
+print(file4 in folder1) #True
+print(file1 in folder2) #False

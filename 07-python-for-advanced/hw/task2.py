1cefbf38e7d4e1bdf89ef8fb45d62a857c93c9de
ataluzz <39642607+ataluzz@users.noreply.github.com>
2019-12-19 10:16:54 +0300

    First update for task2.py

--- 07-python-for-advanced/hw/task2.py
+++ 07-python-for-advanced/hw/task2.py
@@ -5,14 +5,58 @@
       параметром в инициализацию проперти
     * пересчитывает значение, если таймер истек
 """
-
+import datetime
 import time
 import uuid
+
+def timer_property(t):
+    
+    class MyProperty(object):
+        
+        def __init__(self, fget=None, fset=None, fdel=None, doc=None):
+            self.fget = fget
+            self.fset = fset
+            self.fdel = fdel
+            self.time = 0
+            self.__doc__ = doc
+
+        def __get__(self, obj, objtype=None):
+            if obj is None:
+                return self
+            if self.fget is None:
+                raise AttributeError("can't read attr")
+            if datetime.datetime.now() - self.time > datetime.timedelta(seconds = t):
+                self.time = datetime.datetime.now()
+                self.result = self.fget(obj)
+                return self.result
+            return self.result
+
+        def __set__(self, obj, value):
+            if self.fset is None:
+                raise AttributeError("can't set attr")
+            self.time = datetime.datetime.today()
+            self.result = value
+
+        def __delete__(self, obj):
+            if self.fdel is None:
+                raise AttributeError("can't del attr")
+            self.fdel(obj)
+            
+        def getter(self, fget):
+            return type(self)(fget, self.fset, self.fdel, self.__doc__)
+        
+        def setter(self, fset):
+            return type(self)(self.fget, fset, self.fdel, self.__doc__)
+        
+        def deleter(self, fdel):
+            return type(self)(self.fget, self.fset, fdel, self.__doc__)
+    
+    return MyProperty
 
 
 class Message:
 
-    @timer_property(t=10)
+    @timer_property(t=5)
     def msg(self):
         self._msg = self.get_message()
         return self._msg
@@ -25,12 +69,13 @@ class Message:
         """
         Return random string
         """
-        return uuid.uuid4().get_hex()
+        return uuid.uuid4()
 
 
 if __name__ == '__main__':
     m = Message()
-    initial = m.read
-    assert initial is m.read
-    time.sleep(10)
-    assert initial is not m.read
+    m.msg = 'hello'
+    initial = m.msg
+    print(initial is m.msg)
+    time.sleep(6)
+    print(initial is m.msg)

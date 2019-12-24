70d48db9885f21343b6180df7db4055290f6a52e
ataluzz <39642607+ataluzz@users.noreply.github.com>
2019-12-24 20:07:30 +0300

    Update decorator.py

--- 13-design-patterns/hw/1-decorator/decorator.py
+++ 13-design-patterns/hw/1-decorator/decorator.py
@@ -3,20 +3,42 @@
     маршмеллоу, взбитые сливки и сироп, а затем вычислить итоговую стоимость напитка.
 """
 
-
-class Component:
+class Component: 
     def get_cost(self):
         raise NotImplementedError("Override get_cost method")
 
-
-class BaseCoffe(Component):
+class ComponentDecorator(Component):
+    def __init__(self, added_stuff):
+        self.added_stuff = added_stuff
+        
+        
+class BaseCoffee(Component):
     def get_cost(self):
         return 90
 
+        
+class Whip(ComponentDecorator):
+        
+    def get_cost(self):
+        print("You added whipped cream. It'll cost you 30")
+        return self.added_stuff.get_cost() + 30
+
+class Marshmallow(ComponentDecorator):
+        
+    def get_cost(self):
+        print("You added marshmallow. It'll cost you 10")
+        return self.added_stuff.get_cost() + 10
+    
+class Syrup(ComponentDecorator):
+        
+    def get_cost(self):
+        print("You added syrup. It'll cost you 20")
+        return self.added_stuff.get_cost() + 20
+        
 
 if __name__ == "__main__":
-    coffe = BaseCoffe()
-    coffe = Whip(coffe)
-    coffe = Marshmallow(coffe)
-    coffe = Syrup(coffe)
-    print("Итоговая стоимость за кофе: {}".format(str(coffe.get_cost())))
+    coffee = BaseCoffee()
+    coffee = Whip(coffee)
+    coffee = Marshmallow(coffee)
+    coffee = Syrup(coffee)
+    print("Итоговая стоимость за кофе: {}".format(str(coffee.get_cost())))

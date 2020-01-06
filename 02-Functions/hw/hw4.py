f7abe2553e510b4082a664ad43aa8137432c0d9f
ataluzz <39642607+ataluzz@users.noreply.github.com>
2020-01-06 15:13:40 +0300

    Update hw4.py

--- 02-Functions/hw/hw4.py
+++ 02-Functions/hw/hw4.py
@@ -2,19 +2,13 @@ import inspect
 
 def modified_func(func, *fixated_args, **fixated_kwargs):
     def new_function(*args, **kwargs):
-        '''A func implementation of {func_name}
-        with pre-applied arguments being:
-        {fixated_args}
-        {fixated_kwargs}
-        source_code:
-        {source_code}'''
-        
         new_function.__name__ = "func_" + func.__name__
-        new_function.__doc__ = new_function.__doc__.replace('{func_name}', str(new_function.__name__))
-        new_function.__doc__ = new_function.__doc__.replace('{fixated_args}', str(fixated_args))
-        new_function.__doc__ = new_function.__doc__.replace('{fixated_kwargs}', str(fixated_kwargs))
-        new_function.__doc__ = new_function.__doc__.replace('{source_code}', inspect.getsource(new_function))
-
+        new_function.__doc__ = (f'A func implementation of {str(new_function.__name__)} ' 
+                               f'with pre-applied arguments being: '
+                               f'{str(fixated_args)} '
+                               f'{str(fixated_kwargs)} '
+                               f'source_code: '
+                               f'{inspect.getsource(new_function)}')
         all_args = list(fixated_args + args)
         all_kwargs = {**fixated_kwargs, **kwargs}
         all_args = all_args + list(all_kwargs[key] for key in all_kwargs)

7241716a8b52ba9a8ffcbeb1b2234f250b167b8f
ataluzz <39642607+ataluzz@users.noreply.github.com>
2020-01-08 20:30:35 +0300

    Create db.py

---
+++ 14-data-persistence/hw/db.py
@@ -0,0 +1,64 @@
+import json
+import pickle
+from pymongo import MongoClient
+import redis
+
+    
+class Storage:
+    
+    def __init__(self, data, protocol):
+        self.data = data
+        self.protocol = protocol
+    
+    def serialization(self):
+        if self.protocol == 'json':
+            return json.dumps(self.data)
+        elif self.protocol == 'pickle':
+            return pickle.dumps(self.data)
+        else:
+            raise ValueError("Wrong serialization protocol")
+
+    def deserialization(self):
+        if self.protocol == 'json':
+            return json.loads(self.data)
+        elif self.protocol == 'pickle':
+            return pickle.loads(self.data)
+        else:
+            raise ValueError("Wrong serialization protocol")
+            
+    def get_data(self):
+        pass
+    
+    def set_data(self):
+        pass
+            
+    
+class MongoStorage(Storage):
+    def __init__(self, data, protocol, host="localhost", port=27017):
+        super().__init__(data, protocol)
+        self.client = MongoClient(host, port)
+        self.db = self.client.test_database
+        self.collection = self.db.test_collection
+        self.deserial_data = deserialization()
+        self.serial_data = serialization()
+        
+    def get_data(self):
+        return self.collection.find({})
+        
+    def set_data(self):
+        self.collection.insert_one(self.serial_data)
+    
+
+class RedisStorage(Storage):
+    def __init__(self, data, protocol):
+        super().__init__(data, protocol)
+        self.r = redis.Redis()
+        self.serial_data = serialization()
+        self.deserial_data = deserialization()
+        
+    def get_data(self):
+        return self.r.get('protocol'), self.r.get('data')
+        
+    def set_data(self):
+        self.r.set('protocol' + self.protocol)
+        self.r.set('data' + self.serial_data)

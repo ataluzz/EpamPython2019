6a645d9df05600dc9617ff37fc9470407411130a
ataluzz <39642607+ataluzz@users.noreply.github.com>
2020-01-12 18:14:31 +0300

    Update parser.py

--- 10-HTTP/hw/parser.py
+++ 10-HTTP/hw/parser.py
@@ -24,7 +24,6 @@ def find_popular_tags(stories):
     return popular_tags
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6",
